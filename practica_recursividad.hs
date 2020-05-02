83d822ed4938278217a09e2bbf9fc83b1998b38f
Marcos Torres <martorres@est.frba.utn.edu.ar>
2020-05-02 19:21:49 -0300

    solución del punto 4

--- practica_recursividad.hs
+++ practica_recursividad.hs
@@ -8,7 +8,7 @@ Main> promediosAlumnos[(Alumno "juan" [8,6]), (Alumno "maria" [7,9,4]), (Alumno 
 
 type Nombre = String
 type Notas = [Int]
-data Persona = Alumno {nombre::Nombre, notas::Notas}
+data Persona = Alumno {nombre::Nombre, notas::Notas} deriving Show
 
 promedioAlumno :: Persona -> (Nombre, Int)
 promedioAlumno (Alumno nombre notas) = (nombre, div (sum notas) (length notas) ) 
@@ -40,3 +40,10 @@ promediosSinAplazos' lista = filter (\elem -> (div (sum elem) (length elem)) > 5
 
 aprobo :: Persona -> Bool
 aprobo alumno = ((all (>5)). notas) alumno
+
+-- 4. Definir la función aprobaron/1, que dada una lista de alumnos, devuelve los nombres de los alumnos que aprobaron.
+-- Main>aprobaron [Alumno “manuel” [8,6,2,4] , Alumno “elena” [7,9,8,7], Alumno “ana” [6,2,4,2], Alumno “pedro” [9,6,7,10]]
+-- [“elena”, “pedro”]
+
+aprobaron :: [Persona] -> [Nombre]
+aprobaron alumnos = ((map nombre) . (filter aprobo)) alumnos

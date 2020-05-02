cec2d95269f3665e17b3434f1c0dedcea3bf0192
martorres-utn <51000566+martorres-utn@users.noreply.github.com>
2020-05-02 16:32:53 -0300

    agrego comentarios con consignas

--- practica_tipospropios.hs
+++ practica_tipospropios.hs
@@ -1,3 +1,13 @@
+{-
+ 1) Queremos calcular el sueldo de los empleados de nuestra empresa. Tenemos dos tipos de empleado:
+- Los comunes: nos interesa saber el sueldo básico y el nombre.
+- Los jerárquicos: nos interesa conocer el sueldo básico, la cantidad de gente a cargo y el nombre.
+ 
+El sueldo que cobran los comunes se determina por el sueldo básico, en los empleados  jerárquicos se calcula como sueldo básico + plus por la cantidad de gente a cargo (500 por cada persona a cargo).
+ 
+> sueldo( Jefe 15000 3 “Sonia”)
+16500
+ -}
 data Empleado = Comun { nombre::String, sueldoBasico::Double } | 
                 Jefe { nombre::String, sueldoBasico::Double, cantidadACargo::Integer }
 calcularSueldo :: Empleado -> Double
@@ -6,19 +16,30 @@ calcularSueldo (Jefe nombre sueldoBasico cantidadACargo) = sueldoBasico + fromIn
 
 calcularPlus cantidad = cantidad * 500
 
--- data Cafe = { nombre::String, azucar::Double }
--- data Gaseosa = { sabor::String, azucar::Double }
-data Bebida = Cafe { nombreBebida::String, azucar::Double } | Gaseosa { sabor::String, azucar::Double }
+{- 
+2) Se conocen estas bebidas:
+ 
+data Bebida = Cafe Nombre Azucar | Gaseosa Sabor Azucar
 
+Dado un producto determinar si es energizante.
+-      Si es café es energizante si es un capuchino.
+-   Si es una gaseosa es energizante si es de sabor a pomelo y tiene más de 10gr de azúcar.
+>esEnergizante (Gaseosa “pomelo” 12)
+True
+ -}
+
+data Bebida = Cafe { nombreBebida::String, azucar::Double } | Gaseosa { sabor::String, azucar::Double }
 esEnergizante :: Bebida -> Bool
 esEnergizante (Cafe "capuchino" _) = True
 esEnergizante (Gaseosa "pomelo" azucar) = azucar > 10
 esEnergizante _ = False
 
--- 3) Resolver la función find’ que encuentra el primer elemento que cumple una condición. No se puede resolver con recursividad. Si ningún elemento cumple la condición dejar que falle.
--- find’ :: (a -> Bool) -> [a] -> a
---  *Main> find' even [41..339]
--- 42
+{- 
+3) Resolver la función find’ que encuentra el primer elemento que cumple una condición. No se puede resolver con recursividad. Si ningún elemento cumple la condición dejar que falle.
+find’ :: (a -> Bool) -> [a] -> a
+ *Main> find' even [41..339]
+ 42
+ -}
 
 find' :: (a -> Bool) -> [a] -> a
 find' criterio lista = head [elemento | elemento <- lista, criterio elemento]
